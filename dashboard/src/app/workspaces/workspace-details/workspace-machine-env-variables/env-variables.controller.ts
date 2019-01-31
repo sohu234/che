92ac362ccc4c9efe1d206d318c2a9fcbd62fe2f6
Anna Shumilova <ashumilo@redhat.com>
2019-01-31 13:23:18 +0200

    Detect tools containers on dashboard and disable action items for them
    Signed-off-by: Anna Shumilova <ashumilo@redhat.com>

--- dashboard/src/app/workspaces/workspace-details/workspace-machine-env-variables/env-variables.controller.ts
+++ dashboard/src/app/workspaces/workspace-details/workspace-machine-env-variables/env-variables.controller.ts
@@ -13,6 +13,7 @@
 import {ConfirmDialogService} from '../../../../components/service/confirm-dialog/confirm-dialog.service';
 import {IEnvironmentManagerMachine} from '../../../../components/api/environment/environment-manager-machine';
 import {EnvironmentManager} from '../../../../components/api/environment/environment-manager';
+import {CheMachineSourceTypes} from '../../../../components/api/workspace/che-machine-source-types';
 
 interface IEnvironmentVariable {
   name: string;
@@ -42,6 +43,7 @@ export class EnvVariablesController {
   private envVariablesList: Array<IEnvironmentVariable> = [];
   private envVariablesSelectedNumber: number = 0;
   private onChange: Function;
+  private isDefinedContainer: boolean;
 
   /**
    * Default constructor that is using resource
@@ -70,6 +72,9 @@ export class EnvVariablesController {
     if (!selectedMachine || !this.environmentManager) {
       return;
     }
+
+    this.isDefinedContainer = this.selectedMachine.attributes && this.selectedMachine.attributes.source ? this.selectedMachine.attributes.source === CheMachineSourceTypes.RECIPE : false;
+
     this.isEnvVarEditable = this.environmentManager.canEditEnvVariables(selectedMachine);
     this.envVariables = this.environmentManager.getEnvVariables(selectedMachine);
     this.envVariablesList = [];
